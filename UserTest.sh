c155f15c770e73f9a6533728c33544b8761dda3f
liwy255 <150309918+liwy255@users.noreply.github.com>
2024-09-01 23:25:22 +0800

    编辑信息

--- UserTest.sh
+++ UserTest.sh
@@ -2,3 +2,6 @@ curl -X POST \
      -H "Content-Type: application/json" \
      -d '{"userName":"zhangsan","weChatID":"zhangsan2234111","password":"securepassword","avatar":"https://example.com/avatar.jpg","permissionLevel":1}' \
      http://localhost:8000/api/users/register
+
+http://trunet.com/api/users/login
+
