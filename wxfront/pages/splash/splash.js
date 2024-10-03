f62f232bc06f58a98c75726584d98c6e31fd4ff2
liwy255 <150309918+liwy255@users.noreply.github.com>
2024-10-03 20:55:08 +0800

    pages:solved open miniprogram page

--- wxfront/pages/splash/splash.js
+++ wxfront/pages/splash/splash.js
@@ -7,10 +7,32 @@ Page({
   data: {
 
   },
+  /**
+   * 用户点击确定按钮跳转到主页
+   */
   goToIndex: function() {
-    wx.switchTab({
-      url: '/pages/index/index'
-    });
+    if (this.data.selectedFeeling) {
+      // 设置全局变量 feeling 为当前选择的心情
+      getApp().globalData.feeling = this.data.selectedFeeling;
+      wx.switchTab({
+        url: '/pages/index/index'
+      });
+    } else {
+      // 如果没有选择心情，弹出提示对话框
+      wx.showToast({
+        title: '您还没有选择心情哦',
+        icon: 'none',
+        duration: 2000
+      });
+    }
+  },
+  selectMood: function(e) {
+    let feeling = e.currentTarget.dataset.feeling;
+    if (feeling !== this.data.selectedFeeling) {
+      this.setData({
+        selectedFeeling: feeling
+      });
+    }
   },
   /**
    * 生命周期函数--监听页面加载
