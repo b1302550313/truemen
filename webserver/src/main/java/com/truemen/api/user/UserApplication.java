f05d5baac0c5ef140520824df7229a264267a840
lpt <patrickli0917@gmail.com>
2024-10-05 19:53:56 +0800

    把启动类放在了app里，指定不扫描会报错的包

--- webserver/src/main/java/com/truemen/api/user/UserApplication.java
+++ webserver/src/main/java/com/truemen/api/user/UserApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan({ "com.truemen.api.user", "com.truemen.api.common" })
-public class UserApplication {
-    // public static void main(String[] args) {
-    // System.out.println("Hello world!");
-    // }
-    public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
-    }
-}
+//@SpringBootApplication
+//@ComponentScan({ "com.truemen.api.user", "com.truemen.api.common" })
+//public class UserApplication {
+//    // public static void main(String[] args) {
+//    // System.out.println("Hello world!");
+//    // }
+//    public static void main(String[] args) {
+//        SpringApplication.run(UserApplication.class, args);
+//    }
+//}
